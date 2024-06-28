009c7939dd1af55bbf53c97c25a9880dc2ceaa5a
mrkhalnayak <141492925+mrkhalnayak@users.noreply.github.com>
2024-06-28 19:47:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ data "aws_availability_zones" "available" {
 }
 
 locals {
-  cluster_name = "education-eks-${random_string.suffix.result}"
+  cluster_name = "Terraform-cluster-${random_string.suffix.result}"
 }
 
 resource "random_string" "suffix" {
